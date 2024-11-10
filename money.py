c12f20624977c87a8132dc02e900278fc3ce8b3b
cloudcoder88 <btech4104@gmail.com>
2024-11-10 10:56:36 +0100

    Create money.py
    Created with the flask framework and HTML and CSS

---
+++ money.py
@@ -0,0 +1,51 @@
+from flask import Flask
+app =Flask(__name__)
+@app.route('/')
+def index():
+    return """
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sports news</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Welcome to Field Vision</h1>
+  <h1>A place for soccer fans ⚽⚽</h1>
+  <p>This website is dedicated to bringing you the best soccer news and updates ⚽⚽.</p>
+  <p>You can also subscribe to our new YouTube channel to get the latest sports news and updates <a href="https://www.youtube.com/@Fieldvision-k8v">click here</a></p>
+  <div>
+    This page is written by Stephis Bright, a football analyst.
+  </div>
+  <div>
+    Man City lost to Sporting CP this weekend by 4-1 in Lisbon.
+  </div>
+  <img src="https://theanalyst.com/wp-content/uploads/2024/11/sporting-vs-man-city-stats.png" style="width: 500px; height: 300px;">
+  <div>
+    Viktor Gyökeres scored a hat-trick against Man City in Lisbon.
+  </div>
+  <div>
+    Man City have been battling with injuries recently and have lost three matches in a row for the first time since April 2018!
+  </div>
+  <div>
+    "I won't give up, that's for sure," Guardiola said after the loss to Sporting.
+  </div>
+  <div>
+    Manchester City manager Pep Guardiola has playfully dismissed rumors about taking over as Brazil's national team coach after his side's recent 4-1 defeat to Sporting CP in the UEFA Champions League on November 11.
+  </div>
+  <div>
+    Incoming Manchester United boss Ruben Amorim's Sporting produced a stunning second-half comeback to beat Premier League champions Manchester City 4-1, helped by a Viktor Gyokeres hat-trick.
+  </div>
+  <img src="https://cdn.images.express.co.uk/img/dynamic/67/940x/secondary/Man-Utd-are-set-to-back-Ruben-Amorim-early-5748931.avif?r=1730990869152" style="width: 500px; height: 300px;">Manchester United incoming head coach 
+</body>
+</html>
+"""
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+
+
